17e5682fbd5971901d7c55605af4e5606d7d3dd6
robo <bitrecycling@posteo.de>
2018-11-05 13:39:53 +0100

    added "clients" command

--- client-console/src/main/java/de/bitrecycling/timeshizz/clientconsole/commands/Commands.java
+++ client-console/src/main/java/de/bitrecycling/timeshizz/clientconsole/commands/Commands.java
@@ -5,8 +5,18 @@ import de.bitrecycling.timeshizz.clientlib.client.service.ClientService;
 import de.bitrecycling.timeshizz.clientlib.project.service.ProjectService;
 import de.bitrecycling.timeshizz.clientlib.task.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.shell.Shell;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.table.*;
+import reactor.core.Disposable;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 @ShellComponent
 public class Commands {
@@ -19,10 +29,18 @@ public class Commands {
     TaskService taskService;
 
     @ShellMethod("create client")
-    public String create(String name, String address){
+    public String create(String name, String address) {
         Client client = new Client();
         client.setName(name);
         client.setAddress(address);
         return clientService.createClient(client).block().toString();
     }
+
+    @ShellMethod("create client")
+    public List<Client> clients() {
+
+        List<Client> block = clientService.loadAllClients().collectList().block();
+        return block;
+    }
+
 }
