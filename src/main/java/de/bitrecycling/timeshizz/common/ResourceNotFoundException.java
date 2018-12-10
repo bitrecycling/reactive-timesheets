88a68f3e28965517761d5c2141c775aa5f991393
robo <bitrecycling@posteo.de>
2018-12-10 13:15:51 +0100

    added test: resource not found

--- src/main/java/de/bitrecycling/timeshizz/common/ResourceNotFoundException.java
+++ src/main/java/de/bitrecycling/timeshizz/common/ResourceNotFoundException.java
@@ -3,6 +3,9 @@ package de.bitrecycling.timeshizz.common;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * shall be thrown if resource (with given id) is not found. used to produce a 404 response
+ */
 public class ResourceNotFoundException extends ResponseStatusException {
     public ResourceNotFoundException(String resourceName, String id){
         super(HttpStatus.NOT_FOUND, resourceName + " resource with id="+id+" not found");
--- src/test/java/de/bitrecycling/timeshizz/ServiceTests.java
+++ src/test/java/de/bitrecycling/timeshizz/ServiceTests.java
@@ -1,15 +1,10 @@
 package de.bitrecycling.timeshizz;
 
-import de.bitrecycling.timeshizz.client.model.Client;
-import de.bitrecycling.timeshizz.client.repository.ClientRepository;
 import de.bitrecycling.timeshizz.client.service.ClientService;
-import de.bitrecycling.timeshizz.project.model.Project;
-import de.bitrecycling.timeshizz.project.repository.ProjectRespository;
+import de.bitrecycling.timeshizz.common.ResourceNotFoundException;
 import de.bitrecycling.timeshizz.project.service.ProjectService;
-import de.bitrecycling.timeshizz.task.model.Task;
-import de.bitrecycling.timeshizz.task.model.TaskEntry;
-import de.bitrecycling.timeshizz.task.repository.TaskEntryRepository;
-import de.bitrecycling.timeshizz.task.repository.TaskRepository;
+import de.bitrecycling.timeshizz.task.service.TaskEntryService;
+import de.bitrecycling.timeshizz.task.service.TaskService;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,24 +13,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.test.StepVerifier;
 
-import java.time.LocalDateTime;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TimeshizzApplication.class)
 public class ServiceTests {
 
     @Autowired
-    ClientRepository clientRepository;
-    @Autowired
-    ProjectRespository projectRespository;
-    @Autowired
-    TaskRepository taskRepository;
-    @Autowired
-    TaskEntryRepository taskEntryRepository;
-    @Autowired
     ClientService clientService;
     @Autowired
     ProjectService projectService;
+    @Autowired
+    TaskService taskService;
+    @Autowired
+    TaskEntryService taskEntryService;
 
 
     @After
@@ -46,6 +35,17 @@ public class ServiceTests {
     @Test
     public void testContext(){
 
+    }
+
+    /**
+     * tests if exception is thrown if resource not found. The exception is mapped to 404 response
+     */
+    @Test
+    public void testError(){
+        StepVerifier.create(clientService.byId("n/a")).verifyError(ResourceNotFoundException.class);
+        StepVerifier.create(projectService.byId("n/a")).verifyError(ResourceNotFoundException.class);
+        StepVerifier.create(taskService.byId("n/a")).verifyError(ResourceNotFoundException.class);
+        StepVerifier.create(taskEntryService.byId("n/a")).verifyError(ResourceNotFoundException.class);
     }
 
 }
